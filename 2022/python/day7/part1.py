96619eba03dc54a0767bf33fdfc179297b36c793
Atlanswer <atlanswer@gmail.com>
2023-06-06 19:33:54 +0800

    day7 template

---
+++ 2022/python/day7/part1.py
@@ -0,0 +1,53 @@
+"""
+Solving: https://adventofcode.com/2022/day/7
+"""
+
+import time
+from pathlib import Path
+
+from loguru import logger
+
+logger.add("log.log")
+
+AOC_DAY = Path(__file__).parent.name
+INPUT_DIR = Path(__file__).parents[2] / "input"
+INPUT_FILE = INPUT_DIR / AOC_DAY / "input.txt"
+assert INPUT_FILE.exists(), "Input file not present."
+
+
+@logger.catch
+def main() -> None:
+    data = INPUT_FILE.read_text().splitlines()
+
+    data = r"""$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k""".splitlines()
+
+    logger.debug(data)
+
+
+if __name__ == "__main__":
+    t_start = time.perf_counter()
+    main()
+    t_finish = time.perf_counter()
+    logger.info(f"Execution time: {t_finish - t_start:.4f} seconds.")
