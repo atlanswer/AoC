a8c90207fa81bd7f82f1f1dd9bab59a4de760f34
Atlanswer <atlanswer@gmail.com>
2023-06-11 22:38:48 +0800

    new python template with timer decorator

--- 2022/python/template.py
+++ 2022/python/template.py
@@ -3,11 +3,23 @@ Solving: https://adventofcode.com/2022/day/1
 """
 
 import time
+from functools import wraps
 from pathlib import Path
 
 from loguru import logger
 
 logger.add("log.log")
+
+
+def time_this(func):
+    @wraps(func)
+    def timer_wrapper(*args, **kwargs):
+        t_start = time.perf_counter()
+        result = func(*args, **kwargs)
+        t_finish = time.perf_counter()
+        logger.info(f"Execution time: {t_finish - t_start:.4f} seconds.")
+        return result
+    return timer_wrapper
 
 AOC_DAY = Path(__file__).parent.name
 INPUT_DIR = Path(__file__).parents[2] / "input"
@@ -16,6 +28,7 @@ assert INPUT_FILE.exists(), "Input file not present."
 
 
 @logger.catch
+@time_this
 def main() -> None:
     data = INPUT_FILE.read_text()
 
@@ -23,7 +36,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    t_start = time.perf_counter()
     main()
-    t_finish = time.perf_counter()
-    logger.info(f"Execution time: {t_finish - t_start:.4f} seconds.")
