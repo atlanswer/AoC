2db31dfdf627ea8ed95d92802647ceb9ab44a654
Atlanswer <atlanswer@gmail.com>
2023-07-31 13:25:37 +0800

    day12 part1

---
+++ 2022/python/day12/part1.py
@@ -0,0 +1,45 @@
+"""
+Solving: https://adventofcode.com/2022/day/12
+"""
+
+import time
+from functools import wraps
+from pathlib import Path
+
+from loguru import logger
+
+logger.add("log.log")
+
+
+def time_this(func):
+    @wraps(func)
+    def timer_wrapper(*args, **kwargs):
+        t_start = time.perf_counter()
+        result = func(*args, **kwargs)
+        t_finish = time.perf_counter()
+        logger.info(f"Execution time: {t_finish - t_start:.4f} seconds.")
+        return result
+    return timer_wrapper
+
+AOC_DAY = Path(__file__).parent.name
+INPUT_DIR = Path(__file__).parents[2] / "input"
+INPUT_FILE = INPUT_DIR / AOC_DAY / "input.txt"
+assert INPUT_FILE.exists(), "Input file not present."
+
+
+@logger.catch
+@time_this
+def main() -> None:
+    data = INPUT_FILE.read_text()
+    
+    data = r"""Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+
+    logger.debug(data)
+
+
+if __name__ == "__main__":
+    main()
