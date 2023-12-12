71edfb305ded54d0f3e5724f210d2190f616430d
Atlanswer <i@atlanswer.com>
2023-12-12 12:02:17 +0800

    AoC 2023 day1 part1

---
+++ 2023/template.py
@@ -0,0 +1,43 @@
+"""
+Solving: https://adventofcode.com/2023/day/1
+"""
+
+import time
+from functools import wraps
+from pathlib import Path
+
+from loguru import logger
+from typing import Callable
+
+logger.add("log.log")
+
+
+def time_this(func: Callable[..., None]):
+    @wraps(func)
+    def timer_wrapper(*args: None, **kwargs: None):
+        t_start = time.perf_counter()
+        result = func(*args, **kwargs)
+        t_finish = time.perf_counter()
+        logger.info(f"Execution time: {t_finish - t_start:.4f} seconds.")
+        return result
+
+    return timer_wrapper
+
+
+AOC_YEAR = 2023
+AOC_DAY = Path(__file__).parent.name
+INPUT_DIR = Path(__file__).parents[2] / "input"
+INPUT_FILE = INPUT_DIR / AOC_DAY / "input.txt"
+assert INPUT_FILE.exists(), "Input file not present."
+
+
+@logger.catch
+@time_this
+def main() -> None:
+    data = INPUT_FILE.read_text()
+
+    logger.debug(data)
+
+
+if __name__ == "__main__":
+    main()
