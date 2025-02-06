59452646ea162ed2cba10403a6f20b4682dd0295
Atlanswer <atlanswer@gmail.com>
2025-02-06 18:52:00 +0800

    update dependencies

--- 2024/python/day6/part2.py
+++ 2024/python/day6/part2.py
@@ -12,6 +12,8 @@ from rich.logging import RichHandler
 
 ReturnType = TypeVar("ReturnType")
 
+Direction = Literal["Up", "Right", "Down", "Left"]
+
 
 def time_this(func: Callable[..., ReturnType]) -> Callable[..., ReturnType]:
     @wraps(func)
@@ -25,56 +27,100 @@ def time_this(func: Callable[..., ReturnType]) -> Callable[..., ReturnType]:
     return timer_wrapper
 
 
+def debug_print(data: list[list[str]], note: str = "Current loop") -> None:
+    s1 = ["".join(line) for line in data]
+    s2 = "\n".join(s1)
+    logger.debug(f"{note}:\n{s2}")
+
+
+def inbound(data: list[list[str]], r: int, c: int) -> bool:
+    return 0 <= r < len(data) and 0 <= c < len(data[0])
+
+
+def get_next(
+    data: list[list[str]], r: int, c: int, d: Direction
+) -> tuple[int, int, Direction]:
+    match d:
+        case "Up":
+            r, c = r - 1, c
+            if inbound(data, r, c) and data[r][c] == "#":
+                d = "Right"
+        case "Right":
+            r, c = r, c + 1
+            if inbound(data, r, c) and data[r][c] == "#":
+                d = "Down"
+        case "Down":
+            r, c = r + 1, c
+            if inbound(data, r, c) and data[r][c] == "#":
+                d = "Left"
+        case "Left":
+            r, c = r, c - 1
+            if inbound(data, r, c) and data[r][c] == "#":
+                d = "Up"
+
+    return r, c, d
+
+
+def test_loop(data: list[list[str]], r: int, c: int, d: Direction) -> bool:
+    rn, cn, dn = get_next(data, r, c, d)
+
+    while True:
+        debug_print(data, "Testing loop")
+
+        if not inbound(data, rn, cn):
+            break
+
+        if rn == r and cn == c:
+            marker = data[r][c]
+            if (
+                (marker == "^" and dn == "Up")
+                or (marker == ">" and dn == "Right")
+                or (marker == "v" and dn == "Down")
+                or (marker == "<" and dn == "Left")
+            ):
+                return True
+
+        _ = input("Press to progress")
+
+        rn, cn, dn = get_next(data, rn, cn, dn)
+
+    return False
+
+
 @time_this
-def solve(data: str) -> int:
+def solve(data_str: str) -> int:
     res = 0
 
-    data: list[list[str]] = [list(line) for line in data.splitlines()]
+    data: list[list[str]] = [list(line) for line in data_str.splitlines()]
 
-    r_guard, c_guard = 0, 0
-    direction: Literal["Up", "Right", "Down", "Left"] = "Up"
+    r, c = 0, 0
+    d: Direction = "Up"
 
     for r in range(len(data)):
         for c in range(len(data[0])):
             if data[r][c] == "^":
-                r_guard, c_guard = r, c
+                r, c = r, c
                 break
 
-    logger.debug(f"Starting point: [{r_guard}, {c_guard}]")
+    logger.debug(f"Starting point: [{r}, {c}]")
 
-    while 0 <= r_guard < len(data) and 0 <= c_guard < len(data[0]):
-        s1 = ["".join(line) for line in data]
-        s2 = "\n".join(s1)
-        logger.debug(f"{s2}")
-        match direction:
-            case "Up":
-                # logger.debug("Go UP")
-                if r_guard - 1 >= 0 and data[r_guard - 1][c_guard] == "#":
-                    direction = "Right"
-                    continue
-                data[r_guard][c_guard] = "^"
-                r_guard -= 1
-            case "Right":
-                # logger.debug("Go RIGHT")
-                if c_guard + 1 < len(data[0]) and data[r_guard][c_guard + 1] == "#":
-                    direction = "Down"
-                    continue
-                data[r_guard][c_guard] = ">"
-                c_guard += 1
-            case "Down":
-                # logger.debug("Go DOWN")
-                if r_guard + 1 < len(data) and data[r_guard + 1][c_guard] == "#":
-                    direction = "Left"
-                    continue
-                data[r_guard][c_guard] = "v"
-                r_guard += 1
-            case "Left":
-                # logger.debug("Go LEFT")
-                if c_guard - 1 >= 0 and data[r_guard][c_guard - 1] == "#":
-                    direction = "Up"
-                    continue
-                data[r_guard][c_guard] = "<"
-                c_guard -= 1
+    while inbound(data, r, c):
+        debug_print(data)
+
+        rn, cn, dn = get_next(data, r, c, d)
+
+        if inbound(data, rn, cn):
+            match d:
+                case "Up":
+                case "Right":
+                case "Down":
+                case "Left":
+
+        r, c = rn, cn
+
+        data_test = [[i for i in r] for r in data]
+
+        _ = test_loop(data_test, r, c, d)
 
         _ = input("Enter to progress")
 
