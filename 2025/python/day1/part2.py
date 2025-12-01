a193c9473ba694e97174272fcc4aa0e96427a3d4
Atlanswer <atlanswer@gmail.com>
2025-12-01 19:49:17 +0800

    aoc 2025 day1 python solution

---
+++ 2025/python/day1/part2.py
@@ -0,0 +1,61 @@
+"""
+Solving: https://adventofcode.com/2025/day/1#part2
+"""
+
+import re
+from typing import Literal
+
+from aoc import get_input, log, time_this
+
+
+@time_this
+def main():
+    input = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+    input = get_input(__file__)
+
+    dial = 50
+    passwd = 0
+
+    for line in input.splitlines():
+        start_at_0 = True if dial == 0 else False
+
+        match = re.search(r"^([LR])(\d+)$", line)
+        assert match is not None
+
+        direction: Literal["L", "R"] = match.group(1)  # ty:ignore[invalid-assignment]
+        distance: int = int(match.group(2))
+
+        dial = dial - distance if direction == "L" else dial + distance
+        cross, dial = divmod(dial, 100)
+
+        end_at_0 = True if dial == 0 else False
+
+        if cross == 0:
+            if end_at_0:
+                passwd += 1
+        if cross > 0:
+            passwd += cross
+        if cross < 0:
+            passwd += abs(cross)
+            if start_at_0:
+                passwd -= 1
+            if end_at_0:
+                passwd += 1
+
+        # log.debug(f"{cross=}, {dial=}, {passwd=}")
+
+    log.debug(f"{passwd=}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ aoc.py
@@ -0,0 +1,58 @@
+"""Python utilities"""
+
+__version__ = "2025.12.1"
+
+import logging
+import time
+from pathlib import Path
+from typing import Callable, ParamSpec, TypeVar
+
+from rich.logging import RichHandler
+
+# Prettified logger
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+log.addHandler(
+    RichHandler(
+        log_time_format="[%m/%d %H:%M:%S]",
+        markup=True,
+        rich_tracebacks=True,
+        tracebacks_show_locals=True,
+    )
+)
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def time_this(func: Callable[P, R]) -> Callable[P, R]:
+    """Prints execution time."""
+
+    def time_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        t_start = time.perf_counter()
+        res = func(*args, **kwargs)
+        t_end = time.perf_counter()
+
+        duration = t_end - t_start
+        log.debug(f"Execution time: {duration:.3f} s.")
+
+        return res
+
+    return time_wrapper
+
+
+def get_input(cur_file: str) -> str:
+    """Read the input file:
+    ```
+    input = get_input(__file__)
+    ```
+    """
+
+    path = Path(cur_file)
+
+    log.debug(path)
+    AOC_DAY = path.parent.name
+    INPUT_FILE = path.parents[2] / "input" / AOC_DAY / "input.txt"
+    assert INPUT_FILE.exists(), f"Input file ({INPUT_FILE}) not found."
+
+    return INPUT_FILE.read_text()
