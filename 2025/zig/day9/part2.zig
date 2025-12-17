0a958a5029fa2acb61908e199c8bbcd6ad2792e1
Atlanswer <atlanswer@gmail.com>
2025-12-18 01:48:00 +0800

    update

--- 2025/zig/day9/part2.zig
+++ 2025/zig/day9/part2.zig
@@ -26,17 +26,17 @@ const VLines = std.ArrayList(VLine);
 pub fn main() !void {
     defer arena.deinit();
 
-    // const input =
-    //     \\7,1
-    //     \\11,1
-    //     \\11,7
-    //     \\9,7
-    //     \\9,5
-    //     \\2,5
-    //     \\2,3
-    //     \\7,3
-    // ;
-    const input = try getInput();
+    const input =
+        \\7,1
+        \\11,1
+        \\11,7
+        \\9,7
+        \\9,5
+        \\2,5
+        \\2,3
+        \\7,3
+    ;
+    // const input = try getInput();
 
     var input_it = std.mem.splitScalar(u8, std.mem.trim(u8, input, "\n"), '\n');
 
@@ -111,44 +111,115 @@ pub fn main() !void {
     var max_area: usize = 0;
 
     for (positions.items, 0..) |p1, i| {
-        for (positions.items[i + 1 ..]) |p2| {
+        outer: for (positions.items[i + 1 ..]) |p2| {
             const rs, const re = if (p1.r < p2.r) .{ p1.r, p2.r } else .{ p2.r, p1.r };
             const cs, const ce = if (p1.c < p2.c) .{ p1.c, p2.c } else .{ p2.c, p1.c };
+            const cond = rs == 2 and cs == 3 and re == 9 and ce == 5;
+            if (!cond) continue;
+            print("tl: {},{}, br: {},{}\n", .{ rs, cs, re, ce });
+
+            // Horizontal lines
+            {
+                const i_low = searchBetween(.{ .hlines = &horizontal_lines }, rs, .gt);
+                print("i_low: {any}\n", .{i_low});
+                const i_high = searchBetween(.{ .hlines = &horizontal_lines }, re, .lt);
+                print("i_high: {any}\n", .{i_high});
+
+                print("hline: low: {any}, high: {any}\n", .{ i_low, i_high });
+
+                if (i_low != null and i_high != null) {
+                    if (i_low.? > i_high.?) continue;
+                    var idx: usize = i_low.?;
+                    while (idx <= i_high.?) : (idx += 1) {
+                        const hline = horizontal_lines.items[idx];
+                        if (hline.cs <= cs and hline.ce > cs) continue :outer;
+                        if (hline.cs < ce and hline.ce >= ce) continue :outer;
+                    }
+                }
+            }
+            // Vertical lines
+            {
+                const i_low = searchBetween(.{ .vlines = &vertical_lines }, cs, .gt);
+                const i_high = searchBetween(.{ .vlines = &vertical_lines }, ce, .lt);
+
+                print("vline: low: {any}, high: {any}\n", .{ i_low, i_high });
+
+                if (i_low != null and i_high != null) {
+                    if (i_low.? > i_high.?) continue;
+                    var idx: usize = i_low.?;
+                    while (idx <= i_high.?) : (idx += 1) {
+                        const vline = vertical_lines.items[idx];
+                        if (vline.rs <= rs and vline.re > rs) continue :outer;
+                        if (vline.rs < re and vline.re >= re) continue :outer;
+                    }
+                }
+            }
+
+            const area = getArea(p1, p2);
+            if (area > max_area) max_area = area;
         }
     }
+
+    print("max area: {}\n", .{max_area});
 }
 
 fn searchBetween(
-    list: *const union(enum) {
-        vlist: VLines,
-        hlist: HLines,
+    list: union(enum) {
+        vlines: *const VLines,
+        hlines: *const HLines,
         fn len(list: @This()) usize {
             return switch (list) {
-                .vlist => |l| l.items.len,
-                .hlist => |l| l.items.len,
+                .vlines => |l| l.items.len,
+                .hlines => |l| l.items.len,
             };
         }
         fn get(list: @This(), idx: usize) usize {
             assert(idx >= 0);
             assert(idx < list.len());
             return switch (list) {
-                .hlist => |l| l.items[idx],
-                .vlist => |l| l.items[idx],
+                .hlines => |l| l.items[idx].r,
+                .vlines => |l| l.items[idx].c,
             };
         }
     },
-    low: usize,
-    high: usize,
-) error{NotFound}!.{ usize, usize } {
+    context: usize,
+    relation: enum { lt, gt },
+) ?usize {
     var i_start: usize = 0;
-    var i_end: usize = list.len() - 1;
-
-    if (list.get(i_start) > high or list.get(i_end) < low)
-        return error{NotFound};
+    var i_end: usize = list.len();
+    print("i_start: {}, i_end: {}\n", .{i_start, i_end});
 
     while (i_start < i_end) {
-        const mid = (i_end - i_start) / 2;
+        const i_mid = i_start + (i_end - i_start) / 2;
+        print("i_mid: {}\n", .{i_mid});
+
+        switch (relation) {
+            .gt => {
+                if (list.get(i_mid) <= context) {
+                    i_start = i_mid + 1;
+                    continue;
+                }
+                if (i_mid == 0 or list.get(i_mid - 1) <= context) {
+                    return i_mid;
+                } else {
+                    i_end = i_mid;
+                    continue;
+                }
+            },
+            .lt => {
+                if (list.get(i_mid) >= context) {
+                    i_end = i_mid;
+                    continue;
+                }
+                if (i_mid == list.len() - 1 or list.get(i_mid + 1) >= context) {
+                    return i_mid;
+                } else {
+                    i_start = i_mid + 1;
+                }
+            },
+        }
     }
+    return null;
 }
 
 fn getArea(pa: Position, pb: Position) usize {
